4e3840b27f471a7f0035a1227d62e05e8696fa25
miguelmf <953379-miguelmf@users.noreply.gitlab.com>
2023-08-10 14:05:03 +0100

    Added barbar to plugins and added keymaps for barbar

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -22,3 +22,49 @@ vim.cmd('imap <silent><script><expr> <Right> copilot#Accept("<Right>")')
 -- vim.keymap.set("n", "<leader>xl", function() require("trouble").open("loclist") end)
 -- vim.keymap.set("n", "gR", function() require("trouble").open("lsp_references") end)
 vim.cmd("nnoremap <C-i> <cmd>TroubleToggle<cr>")
+
+-- barbar
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+-- Move to previous/next
+map('n', '<C-t>', '<Cmd>tabnew<CR>', opts)
+map('n', '<C-Left>', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<C-Right>', '<Cmd>BufferNext<CR>', opts)
+-- Re-order to previous/next
+-- map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
+-- map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
+-- Goto buffer in position...
+-- map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
+-- map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
+-- map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
+-- map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
+-- map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
+-- map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
+-- map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
+-- map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
+-- map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
+-- map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
+-- Pin/unpin buffer
+-- map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
+-- Close buffer
+map('n', '<C-q>', '<Cmd>BufferClose<CR>', opts)
+-- Wipeout buffer
+--                 :BufferWipeout
+-- Close commands
+--                 :BufferCloseAllButCurrent
+--                 :BufferCloseAllButPinned
+--                 :BufferCloseAllButCurrentOrPinned
+--                 :BufferCloseBuffersLeft
+--                 :BufferCloseBuffersRight
+-- Magic buffer-picking mode
+-- map('n', '<C-p>', '<Cmd>BufferPick<CR>', opts)
+-- -- Sort automatically by...
+-- map('n', '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
+-- map('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>', opts)
+-- map('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
+-- map('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
+
+-- Other:
+-- :BarbarEnable - enables barbar (enabled by default)
+-- :BarbarDisable - very bad command, should never be used
